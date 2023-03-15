71044c54a536027e30ba5c6b6fe463ccb7013d61
Sunil Choudhary <A-HTC4535@mdlz.com>
2023-03-15 21:54:26 +0530

    creating react library using babel and babel.config.json

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import App from "./App.js";
+export default function M(){
+  
+    return (
+      <div>
+        {/* <App /> */}
+        P
+      </div>
+    );
+  };
